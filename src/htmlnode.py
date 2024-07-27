438abb61a6e822141812fc1279c84911a4c1c0bc
Sami-M-H-Aden <adens7066@gmail.com>
2024-07-27 16:41:51 +0100

    Finally fixed all the testing issues

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,8 +11,10 @@ class HTMLNode:
             return ""
         changed_html=""
         for key in self.props:
-            changed_html+=f"{key}={self.props[key]} "
+            changed_html+=f' {key}="{self.props[key]}"'
         return changed_html
     def __repr__(self):
         return f"HTMLNode({self.tag},{self.value},{self.children},{self.props})"
     
+node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
+print(node.props_to_html())
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,14 +2,21 @@ import unittest
 from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
-    def is_eq(self):
-        node_2=HTMLNode("p")
-        self.assertEqual(node_2.props_to_html(),"HTMLNode(p,waaaa,[],{})")
-    def is_none(self):
-        node_3=HTMLNode("p","waaaa")
-        self.assertIsNotNone(node_3.tag)
-    def is_not_none(self):
-        node_3=HTMLNode("p","waaaa")
-        self.assertIsNone(node_3.tag)
+    def test_props_to_html(self):
+        node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
+        expected = ' href="https://www.google.com" target="_blank"'
+        self.assertEqual(node.props_to_html(), expected)
+    
+    def test_tag_is_not_none(self):
+        node = HTMLNode("p", "waaaa")
+        self.assertIsNotNone(node.tag)
+    
+    def test_initialization(self):
+        node = HTMLNode()
+        self.assertIsNone(node.tag)
+        self.assertIsNone(node.value)
+        self.assertIsNone(node.children)
+        self.assertIsNone(node.props)
+
 if __name__ == "__main__":
     unittest.main()
